2763421b3b193cc78f39e722a8d2debd4791727f
Xander Arling <xander.arling@gmail.com>
2014-11-16 17:08:14 +0100

    First test

--- test/karma.conf.js
+++ test/karma.conf.js
@@ -26,6 +26,8 @@ module.exports = function(config) {
       'bower_components/angular-resource/angular-resource.js',
       'bower_components/angular-route/angular-route.js',
       'bower_components/angular-sanitize/angular-sanitize.js',
+      // first load all modules
+      'app/scripts/**/*.module.js',
       'app/scripts/**/*.js',
       'test/mock/**/*.js',
       'test/spec/**/*.js'
